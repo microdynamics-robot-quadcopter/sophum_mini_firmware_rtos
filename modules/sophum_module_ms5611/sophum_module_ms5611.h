651dcea563f50380ca87f3c54c23c1fe82120fae
maksyuki <myc20404401@sina.com>
2019-02-09 17:35:30 +0800

    Del test code

--- modules/sophum_module_ms5611/sophum_module_ms5611.h
+++ modules/sophum_module_ms5611/sophum_module_ms5611.h
@@ -32,20 +32,6 @@
 #define MS5611_BUFFER_SIZE              10
 #define MS5611_PRES_OFFSET_NUM          50
 
-
-#define I2C_EXAMPLE_MASTER_SCL_IO          18               /*!< gpio number for I2C master clock */
-#define I2C_EXAMPLE_MASTER_SDA_IO          19               /*!< gpio number for I2C master data  */
-#define I2C_EXAMPLE_MASTER_NUM             I2C_NUM_1        /*!< I2C port number for master dev */
-#define I2C_EXAMPLE_MASTER_TX_BUF_DISABLE  0                /*!< I2C master do not need buffer */
-#define I2C_EXAMPLE_MASTER_RX_BUF_DISABLE  0                /*!< I2C master do not need buffer */
-#define I2C_EXAMPLE_MASTER_FREQ_HZ         100000           /*!< I2C master clock frequency */
-
-#define ACK_CHECK_EN                       0x1              /*!< I2C master will check ack from slave*/
-#define ACK_CHECK_DIS                      0x0              /*!< I2C master will not check ack from slave */
-#define ACK_VAL                            0x0              /*!< I2C ack value */
-#define NACK_VAL                           0x1              /*!< I2C nack value */
-
-
 /* public variable */
 extern uint16_t calibration_params[MS5611_PROM_REG_COUNT+2];
 

f5703f72469e6afa0dfdfc6e0ef65ce905463b67
maksyuki <myc20404401@sina.com>
2019-03-05 15:24:33 +0800

    Add user write, read and cali alti func in BMP388 module

--- modules/sophum_module_bmp388/sophum_module_bmp388.c
+++ modules/sophum_module_bmp388/sophum_module_bmp388.c
@@ -1,17 +1,35 @@
 #include "sophum_module_bmp388.h"
 
 
-struct bmp3_dev bmp388_dev;
-int8_t ret = BMP3_OK;
+int8_t BMP388_readMultiBytes(uint8_t dev_id, uint8_t reg_addr, uint8_t *data, uint16_t len)
+{
+    int8_t ret = 0;
+    ret = I2C_readMultiBytes(I2C_NUM1_MASTER_PORT_GPIO, dev_id << 1, reg_addr, len, data);
+    if(ret == true)
+    {
+        return BMP3_OK;
+    }
+    else
+    {
+        return BMP3_E_COMM_FAIL;
+    }
+}
 
-// bmp388_dev.dev_id   = BMP3_I2C_ADDR_PRIM;
-// bmp388_dev.intf     = BMP3_I2C_INTF;
-// bmp388_dev.read     = ;
-// bmp388_dev.write    = ;
-// bmp388_dev.delay_ms = SOPHUM_delayMs;
+int8_t BMP388_writeMultiBytes(uint8_t dev_id, uint8_t reg_addr, uint8_t *data, uint16_t len)
+{
+    int8_t ret = 0;
+    ret = I2C_writeMultiBytes(I2C_NUM1_MASTER_PORT_GPIO, dev_id << 1, reg_addr, len, data);
+    if(ret == true)
+    {
+        return BMP3_OK;
+    }
+    else
+    {
+        return BMP3_E_COMM_FAIL;
+    }
+}
 
-
-// int8_t BMP388_readOneByte(uint8_t dev_addr, uint8_t reg_addr, )
-// {
-
-// }
+extern double BMP388_calcAltitude(double pres)
+{
+    return (1.0F - powf(pres / 101325, 0.190284F)) * 287.15F / 0.0065F;
+}
--- modules/sophum_module_bmp388/sophum_module_bmp388.h
+++ modules/sophum_module_bmp388/sophum_module_bmp388.h
@@ -1,9 +1,28 @@
 #ifndef __SOPHUM_MODULE_BMP388_H__
 #define __SOPHUM_MODULE_BMP388_H__
 
+#include <math.h>
 #include "bmp3.h"
 #include "bmp3_defs.h"
 #include "sophum_driver_sys.h"
 #include "sophum_driver_i2c.h"
+
+
+
+extern int8_t BMP388_readMultiBytes(uint8_t dev_id,  uint8_t reg_addr, uint8_t *data, uint16_t len);
+extern int8_t BMP388_writeMultiBytes(uint8_t dev_id, uint8_t reg_addr, uint8_t *data, uint16_t len);
+extern double BMP388_calcAltitude(double pres);
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif

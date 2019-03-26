73eabc0769c847360fa14d6505e807e295868b82
maksyuki <myc20404401@sina.com>
2019-03-26 14:57:00 +0800

    Add optical flow module

---
+++ modules/sophum_module_upixels/sophum_module_upixels.h
@@ -0,0 +1,11 @@
+#ifndef __SOPHUM_MODULE_UPIXELS_H__
+#define __SOPHUM_MODULE_UPIXELS_H__
+
+#include "sophum_driver_sys.h"
+#include "sophum_driver_uart.h"
+
+
+extern void UPIXELS_Init(void);
+extern void UPIXELS_updateData(void);
+
+#endif

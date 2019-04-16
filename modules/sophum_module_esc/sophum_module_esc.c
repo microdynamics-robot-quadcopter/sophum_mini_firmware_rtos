ed3307aa222ff96566695df2bcfaa596a6df56ef
maksyuki <myc20404401@sina.com>
2019-04-16 22:39:45 +0800

    Add pwm standard esc code

--- modules/sophum_module_esc/sophum_module_esc.c
+++ modules/sophum_module_esc/sophum_module_esc.c
@@ -1,5 +1,8 @@
 #include "sophum_module_esc.h"
 
+static uint16_t esc_pwm_output[ESC_TX_CHANNEL_NUM] = {400, 400, 400, 400};
+static ledc_channel_config_t esc_pwm_channel[ESC_TX_CHANNEL_NUM];
+
 const rmt_item32_t esc_bit0 = {{{24, 1, 50, 0 }}};
 const rmt_item32_t esc_bit1 = {{{50, 1, 25, 0 }}};
 
@@ -108,7 +111,68 @@ static uint16_t ESC_limitRange(uint16_t val, uint16_t minval, uint16_t maxval)
 
 static void ESC_initPWMStandard(void)
 {
+    /* Prepare and set configuration of timers that will be used by LED Controller */
+    ledc_timer_config_t esc_pwm_timer =
+    {
+        .duty_resolution = ESC_PWM_RESOLUTION, /* resolution of PWM duty */
+        .freq_hz = ESC_PWM_FREQ,               /* frequency of PWM signal */
+        .speed_mode = ESC_PWM_MODE,            /* timer mode */
+        .timer_num  = ESC_PWM_TIMER            /* timer index */
+    };
+    /* Set configuration of timer0 for high speed channels */
+    ledc_timer_config(&esc_pwm_timer);
 
+    for(int i = 0; i < ESC_TX_CHANNEL_NUM; i++)
+    {
+        esc_pwm_channel[i].duty = 0;
+        esc_pwm_channel[i].speed_mode = ESC_PWM_MODE;
+        esc_pwm_channel[i].timer_sel  = ESC_PWM_TIMER;
+        if(i == 0)
+        {
+            esc_pwm_channel[i].channel  = ESC_PWM_CHANNEL0;
+            esc_pwm_channel[i].gpio_num = ESC_TX_GPIO0;
+        }
+        if(i == 1)
+        {
+            esc_pwm_channel[i].channel  = ESC_PWM_CHANNEL1;
+            esc_pwm_channel[i].gpio_num = ESC_TX_GPIO1;
+        }
+        if(i == 2)
+        {
+            esc_pwm_channel[i].channel  = ESC_PWM_CHANNEL2;
+            esc_pwm_channel[i].gpio_num = ESC_TX_GPIO2;
+        }
+        if(i == 3)
+        {
+            esc_pwm_channel[i].channel  = ESC_PWM_CHANNEL3;
+            esc_pwm_channel[i].gpio_num = ESC_TX_GPIO3;
+        }
+    }
+
+    /* Set LED Controller with previously prepared configuration */
+    for(int i = 0; i < ESC_TX_CHANNEL_NUM; i++)
+    {
+        ledc_channel_config(&esc_pwm_channel[i]);
+    }
+
+    int32_t duty_val = 500;
+    for(int i = 1; i <= 2; i++)
+    {
+        for(int j = 0; j < ESC_TX_CHANNEL_NUM; j++)
+        {
+            ledc_set_duty(esc_pwm_channel[j].speed_mode, esc_pwm_channel[j].channel, duty_val);
+            ledc_update_duty(esc_pwm_channel[j].speed_mode, esc_pwm_channel[j].channel);
+            while(ledc_get_duty(esc_pwm_channel[j].speed_mode, esc_pwm_channel[j].channel) != duty_val)
+            {
+                // printf("set duty to %d...\n", duty_val);
+            }
+        }
+        // printf("has set duty to %d\n", duty_val);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        duty_val -= 100;
+    }
+    vTaskDelay(1500 / portTICK_PERIOD_MS);
+    // printf("esc init successful(pwm 1~2ms)\n");
 }
 
 /* val: [48~2048] */
@@ -221,7 +285,7 @@ void ESC_Init(ESC_MotorProtocolTypes protocol_type)
     }
 }
 
-/* output_ch*: [1000~2000] min: about 1088 */
+/* output_ch*: [1000~2000](ms) (pwm: [400,1024]) (dshot: [1088,2048]) */
 void ESC_updateOutput(ESC_MotorProtocolTypes protocol_type, uint16_t output_ch0,
                       uint16_t output_ch1, uint16_t output_ch2, uint16_t output_ch3)
 {
@@ -232,7 +296,10 @@ void ESC_updateOutput(ESC_MotorProtocolTypes protocol_type, uint16_t output_ch0,
 
     if(protocol_type == PWM_TYPE_STANDARD)
     {
-
+        esc_pwm_output[0] = output_ch0 - 1000;
+        esc_pwm_output[1] = output_ch1 - 1000;
+        esc_pwm_output[2] = output_ch2 - 1000;
+        esc_pwm_output[3] = output_ch3 - 1000;
     }
     else if(protocol_type == PWM_TYPE_DSHOT600)
     {
@@ -249,7 +316,19 @@ void ESC_updateOutput(ESC_MotorProtocolTypes protocol_type, uint16_t output_ch0,
 
 void ESC_txTask(ESC_MotorProtocolTypes protocol_type)
 {
-    if(protocol_type == PWM_TYPE_STANDARD){}
+    if(protocol_type == PWM_TYPE_STANDARD)
+    {
+        for(int i = 0; i < ESC_TX_CHANNEL_NUM; i++)
+        {
+            ledc_set_duty(esc_pwm_channel[i].speed_mode, esc_pwm_channel[i].channel, esc_pwm_output[i]);
+            ledc_update_duty(esc_pwm_channel[i].speed_mode, esc_pwm_channel[i].channel);
+            while(ledc_get_duty(esc_pwm_channel[i].speed_mode, esc_pwm_channel[i].channel) != esc_pwm_output[i])
+            {
+                // printf("set duty to %d...\n", duty_val);
+            }
+            // printf("has set duty to %d\n", duty_val);
+        }
+    }
     else if(protocol_type == PWM_TYPE_DSHOT600)
     {
         while(1)
